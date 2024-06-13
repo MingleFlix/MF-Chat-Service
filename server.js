1f01e0c75ed89378ce300f6e2902efbcc0f024cf
Pham Van <phamvan.nguyen@haw-hamburg.de>
2024-06-13 19:24:05 +0200

    Basic Chat without any User-Managment-Service

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
+app.use(express.static('public'));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000", // Passe die URL an deine React-App an
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
@@ -17,9 +19,15 @@ const io = socketIo(server, {
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  socket.on('join', (username) => {
-    console.log(`${username} joined the chat`);
-    socket.broadcast.emit('message', { user: 'admin', text: `${username} has joined the chat` });
+  socket.on('join', async (username) => {
+    try {
+      const response = await axios.get(`http://localhost:5000/users/${username}`);
+      const user = response.data;
+      console.log(`${user.name} joined the chat`);
+      socket.broadcast.emit('message', { user: 'admin', text: `${user.name} has joined the chat` });
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
   });
 
   socket.on('message', (message) => {
